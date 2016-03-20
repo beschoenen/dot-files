addc72388d3adc2721a1abda7e8d6c24dc6229f1
Kevin Richter <me@kevinrichter.nl>
2016-03-20 19:08:57 +0100

    Initial commit

---
+++ OS X/brew.sh
@@ -0,0 +1,13 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew doctor
+
+brew update
+
+brew tap caskroom/cask
+
+brew tap caskroom/versions
+
+# TODO install packages
+
+# TODO install applications 
